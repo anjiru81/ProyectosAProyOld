f979b1c063e1989bd536c3880debb8358135eeff
anjiru81 <anjiru81@gmail.com>
2014-10-01 14:06:58 +0200

    Con dialogo para propiedades

---
+++ ProyectosAProyOld/src/proyectosAproyOld/ConfigPropsDiag.java
@@ -0,0 +1,112 @@
+package proyectosAproyOld;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import java.awt.GridLayout;
+import javax.swing.SwingConstants;
+import javax.swing.BoxLayout;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.LayoutStyle.ComponentPlacement;
+
+public class ConfigPropsDiag extends JDialog {
+
+	private final JPanel contentPanel = new JPanel();
+	private JTextField textField;
+	private JLabel lblRutaOrigen;
+	private JTextField textField_1;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		try {
+			ConfigPropsDiag dialog = new ConfigPropsDiag();
+			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+			dialog.setVisible(true);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Create the dialog.
+	 */
+	public ConfigPropsDiag() {
+		setBounds(100, 100, 450, 214);
+		getContentPane().setLayout(new BorderLayout());
+		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		{
+			lblRutaOrigen = new JLabel("Ruta Origen 1");
+			lblRutaOrigen.setHorizontalTextPosition(SwingConstants.CENTER);
+		}
+		{
+			textField = new JTextField();
+			textField.setColumns(40);
+		}
+		
+		JLabel label = new JLabel("Ruta Origen 1");
+		label.setHorizontalTextPosition(SwingConstants.CENTER);
+		
+		textField_1 = new JTextField();
+		textField_1.setColumns(40);
+		GroupLayout gl_contentPanel = new GroupLayout(contentPanel);
+		gl_contentPanel.setHorizontalGroup(
+			gl_contentPanel.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPanel.createSequentialGroup()
+					.addComponent(lblRutaOrigen, GroupLayout.PREFERRED_SIZE, 100, GroupLayout.PREFERRED_SIZE)
+					.addGap(0, 0, Short.MAX_VALUE)
+					.addComponent(textField, GroupLayout.PREFERRED_SIZE, 216, GroupLayout.PREFERRED_SIZE)
+					.addGap(116))
+				.addGroup(gl_contentPanel.createSequentialGroup()
+					.addComponent(label, GroupLayout.PREFERRED_SIZE, 100, GroupLayout.PREFERRED_SIZE)
+					.addComponent(textField_1, GroupLayout.PREFERRED_SIZE, 216, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap())
+		);
+		gl_contentPanel.setVerticalGroup(
+			gl_contentPanel.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPanel.createSequentialGroup()
+					.addContainerGap()
+					.addGroup(gl_contentPanel.createParallelGroup(Alignment.LEADING)
+						.addComponent(lblRutaOrigen, GroupLayout.PREFERRED_SIZE, 25, GroupLayout.PREFERRED_SIZE)
+						.addComponent(textField, GroupLayout.DEFAULT_SIZE, 25, Short.MAX_VALUE))
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addGroup(gl_contentPanel.createParallelGroup(Alignment.LEADING)
+						.addGroup(gl_contentPanel.createSequentialGroup()
+							.addGap(42)
+							.addComponent(label, GroupLayout.PREFERRED_SIZE, 25, GroupLayout.PREFERRED_SIZE))
+						.addGroup(gl_contentPanel.createSequentialGroup()
+							.addGap(42)
+							.addComponent(textField_1, GroupLayout.PREFERRED_SIZE, 25, GroupLayout.PREFERRED_SIZE)))
+					.addGap(70))
+		);
+		contentPanel.setLayout(gl_contentPanel);
+		{
+			JPanel buttonPane = new JPanel();
+			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
+			getContentPane().add(buttonPane, BorderLayout.SOUTH);
+			{
+				JButton okButton = new JButton("OK");
+				okButton.setActionCommand("OK");
+				buttonPane.add(okButton);
+				getRootPane().setDefaultButton(okButton);
+			}
+			{
+				JButton cancelButton = new JButton("Cancel");
+				cancelButton.setActionCommand("Cancel");
+				buttonPane.add(cancelButton);
+			}
+		}
+	}
+}
---
+++ ProyectosAProyOld/src/proyectosAproyOld/ProyOldProperties.java
@@ -0,0 +1,64 @@
+package proyectosAproyOld;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.LinkedList;
+import java.util.Properties;
+
+public class ProyOldProperties {
+
+	private LinkedList<String> rutas_origen;
+	private String ruta_destino;
+	public ProyOldProperties() {
+		 rutas_origen = new LinkedList<String>();
+		 ruta_destino = "";
+	}
+	public void loadParams() {
+	    Properties propiedades = new Properties();
+	    InputStream is = null;
+	 
+	    // First try loading from the current directory
+	    try {
+	        File f = new File("configuracion.properties");
+	        is = new FileInputStream( f );
+	    }
+	    catch ( Exception e ) { is = null; }
+	 
+	    try {
+	        if ( is == null ) {
+	            // Try loading from classpath
+	            is = getClass().getResourceAsStream("configuracion.properties");
+	        }
+	 
+	        // Try loading properties from the file (if found)
+	        propiedades.load( is );
+	        int num_rutas=Integer.parseInt(propiedades.getProperty("num_rutas"));
+	        for(int i=0;i<num_rutas;i++){
+	        	rutas_origen.add(propiedades.getProperty("ruta"+i));
+	        }
+	        ruta_destino = propiedades.getProperty("ruta_destino");
+	    }
+	    catch ( Exception e ) { }
+	   
+	}
+	public void saveParamChanges() {
+	    try {
+	        Properties props = new Properties();
+	        String serverAddr = null;
+			props.setProperty("ServerAddress", serverAddr);
+	        String serverPort = null;
+			props.setProperty("ServerPort", ""+serverPort);
+	        String threadCnt = null;
+			props.setProperty("ThreadCount", ""+threadCnt);
+	        File f = new File("configuracion.properties");
+	        OutputStream out = new FileOutputStream( f );
+	        props.store(out, "This is an optional header comment string");
+	    }
+	    catch (Exception e ) {
+	        e.printStackTrace();
+	    }
+	}
+}
